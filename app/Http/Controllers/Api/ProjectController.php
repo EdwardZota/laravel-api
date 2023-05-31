d514b13a839452aa6c05e5c5c9e9929d52e7e6ea
Edward Zota <zotaedward@gmail.com>
2023-05-31 17:13:29 +0200

    esercizio concluso

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,11 +9,21 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with(['type','technologies'])->get();
+        $projects = Project::with(['type','technologies'])->paginate(3);
 
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
-}
+    }
+
+    public function show($post_slug){
+        $project = Project::where('post_slug',$post_slug)->with(['type','technologies'])->first();
+
+        return response()->json([
+            'success' => true,
+            'project' => $project
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/projects',[ProjectController::class,'index']);
+
+Route::get('/project/{post_slug}',[ProjectController::class,'show']);
+
